cdb38f704fb18d6a408ff90347bac93cbb5445d0
fullife32 <eithan.assouline6@gmail.com>
2023-03-20 04:46:55 +0100

    switching to alpine os

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
-apt update && apt upgrade -y
-apt install wget openjdk-17-jre-headless -y
+apk update && apk upgrade
+apk add wget openjdk17-jre-headless
 cd /minecraft
 FILE=server.jar
 if [ ! -f "$FILE" ]; then
     wget https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar server.jar
 fi
+echo "eula=true" > eula.txt
 java -Xmx1024M -Xms1024M -jar server.jar nogui
+
+# apt update && apt upgrade -y
+# apt install wget openjdk-17-jre-headless -y
+# cd /minecraft
+# FILE=server.jar
+# if [ ! -f "$FILE" ]; then
+#     wget https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar server.jar
+# fi
+# echo "eula=true" > eula.txt
+# java -Xmx1024M -Xms1024M -jar server.jar nogui
